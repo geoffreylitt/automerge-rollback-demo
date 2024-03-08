510eb0fe68c7ee66bf2fbb8fcb18b8423c4dd3d0
Geoffrey Litt <gklitt@gmail.com>
2024-03-07 22:23:39 -0500

    clean up tests

--- test/rollback.test.js
+++ test/rollback.test.js
@@ -9,12 +9,10 @@ it("does a simple rollback", () => {
     title: "title 1",
   });
 
-  // Update the data (we get back a new doc)
   const doc2 = A.change(doc1, (d) => {
     d.title = "title 2";
   });
 
-  // Update data again (get back yet another new doc)
   const doc3 = A.change(doc2, (d) => {
     d.title = "title 3";
   });
@@ -22,9 +20,36 @@ it("does a simple rollback", () => {
   const rolledBackDoc = rollbackDoc(doc3, A.getHeads(doc2));
 
   expect(rolledBackDoc).toEqual(doc2);
+
+  // note: the rolled back doc has more changes than the previous doc.
+  // we've added a revert change, we haven't "reverted" to the previous changeset.
   expect(A.getAllChanges(rolledBackDoc).length).toBeGreaterThan(
     A.getAllChanges(doc3).length
   );
+});
+
+it("handles multiple fields being set", () => {
+  const doc1 = A.from({
+    title: "title 1",
+    content: "content 1",
+    resolved: false,
+  });
+
+  const doc2 = A.change(doc1, (d) => {
+    d.title = "title 2";
+    d.content = "content 2";
+    d.resolved = true;
+  });
+
+  const doc3 = A.change(doc2, (d) => {
+    d.title = "title 3";
+    d.content = "content 3";
+    d.resolved = false;
+  });
+
+  const rolledBackDoc = rollbackDoc(doc3, A.getHeads(doc2));
+
+  expect(rolledBackDoc).toEqual(doc2);
 });
 
 it("handles multiple fields being set", () => {
@@ -52,55 +77,19 @@ it("handles multiple fields being set", () => {
   const rolledBackDoc = rollbackDoc(doc3, A.getHeads(doc2));
 
   expect(rolledBackDoc).toEqual(doc2);
-  expect(A.getAllChanges(rolledBackDoc).length).toBeGreaterThan(
-    A.getAllChanges(doc3).length
-  );
-});
-
-it("handles multiple fields being set", () => {
-  // Make an initial doc
-  const doc1 = A.from({
-    title: "title 1",
-    content: "content 1",
-    resolved: false,
-  });
-
-  // Update the data (we get back a new doc)
-  const doc2 = A.change(doc1, (d) => {
-    d.title = "title 2";
-    d.content = "content 2";
-    d.resolved = true;
-  });
-
-  // Update data again (get back yet another new doc)
-  const doc3 = A.change(doc2, (d) => {
-    d.title = "title 3";
-    d.content = "content 3";
-    d.resolved = false;
-  });
-
-  const rolledBackDoc = rollbackDoc(doc3, A.getHeads(doc2));
-
-  expect(rolledBackDoc).toEqual(doc2);
-  expect(A.getAllChanges(rolledBackDoc).length).toBeGreaterThan(
-    A.getAllChanges(doc3).length
-  );
 });
 
 it("handles array insertions", () => {
-  // Make an initial doc
   const doc1 = A.from({
     items: [1, 2, 3, 4, 5],
   });
 
-  // Update the data (we get back a new doc)
   const doc2 = A.change(doc1, (d) => {
     d.items.splice(2, 0, 6); // Insert 6 at index 2
     d.items.push(7); // Insert 7 at the end
     d.items.unshift(0); // Insert 0 at the beginning
   });
 
-  // Update data again (get back yet another new doc)
   const doc3 = A.change(doc2, (d) => {
     d.items.splice(2, 0, 6); // Insert 6 at index 2
     d.items.push(7); // Insert 7 at the end
@@ -110,23 +99,17 @@ it("handles array insertions", () => {
   const rolledBackDoc = rollbackDoc(doc3, A.getHeads(doc2));
 
   expect(rolledBackDoc).toEqual(doc2);
-  expect(A.getAllChanges(rolledBackDoc).length).toBeGreaterThan(
-    A.getAllChanges(doc3).length
-  );
 });
 
 it("handles string editing", () => {
-  // Make an initial doc
   const doc1 = A.from({
     content: "initial content",
   });
 
-  // Update the data (we get back a new doc)
   const doc2 = A.change(doc1, (d) => {
     A.splice(d, ["content"], 8, 7, "words");
   });
 
-  // Update data again (get back yet another new doc)
   const doc3 = A.change(doc2, (d) => {
     A.splice(d, ["content"], 8, 5, "string stuff");
   });
@@ -134,7 +117,4 @@ it("handles string editing", () => {
   const rolledBackDoc = rollbackDoc(doc3, A.getHeads(doc2));
 
   expect(rolledBackDoc).toEqual(doc2);
-  expect(A.getAllChanges(rolledBackDoc).length).toBeGreaterThan(
-    A.getAllChanges(doc3).length
-  );
 });
